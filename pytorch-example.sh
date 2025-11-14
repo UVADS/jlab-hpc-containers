a9467c03876cf5f1e0b0e4c01ea0f2e22f417018
khs3z <khs3z@virginia.edu>
2025-11-14 07:23:36 -0500

    updated paths

--- pytorch-example.sh
+++ pytorch-example.sh
@@ -8,6 +8,9 @@
 #SBATCH -e slurm-%j.err                 # error file
 #SBATCH -o slurm-%j.out                 # stdout file
 
+# get example Py script
+curl -o pytorch-example.py https://raw.githubusercontent.com/pytorch/examples/refs/heads/main/mnist/main.py
+
 module purge
 module load apptainer
 apptainer exec --nv --bind ~/local/pytorch-2.9.1:$HOME/.local ~/pytorch-2.9.1.sif python pytorch-example.py
